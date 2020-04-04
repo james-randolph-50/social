c94cb4a0f590edac3a7b908c43351cee13639f48
James Randolph <james.randolph2016@gmail.com>
2020-04-04 09:33:22 -0400

    test

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "social");
+if(mysqli_connect_errno()) {
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
+?>
+
+
+<html>
+<head>
+<title>Social network</title>
+</head>
+<body>
+yo gotti
+
+</html>
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,41 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "social");
+if(mysqli_connect_errno()) {
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+// Declaring vars to prevent errors
+$fname = "";
+$lname = "";
+$em = "";
+$em2 = "";
+$password = "";
+$password2 = "";
+$date = "";
+$error_array = "";
+?>
+
+<html>
+<head>
+<title>Register</title>
+</head>
+<body>
+<h1>Register</h1>
+<form action="register.php" method="POST">
+    <input type="text" name="reg_fname" placeholder="First Name" required>
+    <br>
+    <input type="text" name="reg_lname" placeholder="Last Name" required>
+    <br>
+    <input type="email" name="reg_email" placeholder="Email" required>
+    <br>
+    <input type="email" name="reg_email2" placeholder="Confirm Email" required>
+    <br>
+    <input type="password" name="reg_password" placeholder="Password" required>
+    <br>
+    <input type="password" name="reg_password2" placeholder="Confirm Password" required>
+    <br>
+    <input type="submit" name="register_button" value="Register">
+</form>
+</body>
+
+</html>
