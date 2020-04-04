e68c6be399940f152731a0b181468652d8bd17aa
James Randolph <james.randolph2016@gmail.com>
2020-04-04 11:33:25 -0400

    partials added

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "social");
-if(mysqli_connect_errno()) {
-    echo "Failed to connect: " . mysqli_connect_errno();
-}
+require 'config/config.php';
+
 ?>
 
 
