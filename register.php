752aaa2019fe5e07bfa96a07ed9d9177738d21f7
James Randolph <james.randolph2016@gmail.com>
2020-04-04 10:20:35 -0400

    session variables added to rest of form save passwords

--- register.php
+++ register.php
@@ -101,17 +101,29 @@ if(isset($_POST['register_button'])) {
 <body>
 <h1>Register</h1>
 <form action="register.php" method="POST">
-    <input type="text" name="reg_fname" placeholder="First Name"value="<?php 
+    <input type="text" name="reg_fname" placeholder="First Name" value="<?php 
         if(isset($_SESSION['reg_fname'])) {
             echo $_SESSION['reg_fname'];
         }
     ?>" required>
     <br>
-    <input type="text" name="reg_lname" placeholder="Last Name" required>
+    <input type="text" name="reg_lname" placeholder="Last Name" value="<?php 
+        if(isset($_SESSION['reg_lname'])) {
+            echo $_SESSION['reg_lname'];
+        }
+    ?>" required>
     <br>
-    <input type="email" name="reg_email" placeholder="Email" required>
+    <input type="email" name="reg_email" placeholder="Email" value="<?php 
+        if(isset($_SESSION['reg_email'])) {
+            echo $_SESSION['reg_email'];
+        }
+    ?>" required>
     <br>
-    <input type="email" name="reg_email2" placeholder="Confirm Email" required>
+    <input type="email" name="reg_email2" placeholder="Confirm Email" value="<?php 
+        if(isset($_SESSION['reg_email2'])) {
+            echo $_SESSION['reg_email2'];
+        }
+    ?>"  required>
     <br>
     <input type="password" name="reg_password" placeholder="Password" required>
     <br>
