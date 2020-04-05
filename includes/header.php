43073671cf69582e947cf131be924862f2710c19
James Randolph <james.randolph2016@gmail.com>
2020-04-05 09:46:02 -0400

    nac complete

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,8 @@ require 'config/config.php';
 
 if(isset($_SESSION['username'])) {
     $userLoggedIn = $_SESSION['username'];
+    $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+    $user = mysqli_fetch_array($user_details_query);
 } else {
     header("Location: register.php");
 }
@@ -36,6 +38,7 @@ if(isset($_SESSION['username'])) {
 
     <nav>
     
+        <a href="#"><?php echo $user['first_name']; ?></a>
         <a href="#"><i class="fa fa-home"></i></a>
         <a href="#"><i class="fa fa-envelope"></i></a>
         <a href="#"><i class="fa fa-users"></i></a>
