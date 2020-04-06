87e2178403a34fb84fa4e2162f8538bf83767bbf
James Randolph <james.randolph2016@gmail.com>
2020-04-06 18:50:43 -0400

    timestamps

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -79,10 +79,32 @@ class Post {
              $date_time_now = date("Y-m-d H:i:s");
              $start_date = new DateTime($date_time);
              $end_date = new DateTime($date_time_now);
+             $interval = $start_date->diff($end_date);
+             if($interval->y >= 1) {
+                if($inteval == 1)
+                    $time_message = $interval->y . " year ago";
+                else
+                    $time_message = $interval->y . " years ago";
+             }
+             else if ($interval-> m >= 1) {
+                 if($interval->d == 0) {
+                     $days = " ago";
+                 }
+                 else if($interval->d == 1) {
+                    $days = $interval->d . " day ago"
+                 }
+                 else {
+                    $days = $interval->d . " days ago"
+                 }
 
+                 if($interval->m == 1) {
+                     $time_message = $interval->m . " month" . $days;
+                 } else {
+                    $time_message = $interval->m . " months" . $days;
+                 }
+             }
         }
     }
-
 }
 
 ?>
