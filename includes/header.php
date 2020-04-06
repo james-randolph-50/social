5e383960fabad0d17cf0ec1f72dbaccac8fbbbd1
James Randolph <james.randolph2016@gmail.com>
2020-04-06 15:05:13 -0400

    links to user profile set

--- includes/header.php
+++ includes/header.php
@@ -37,7 +37,7 @@ if(isset($_SESSION['username'])) {
     </div>
 
     <nav>
-        <a href="#"><?php echo $user['first_name']; ?></a>
+        <a href="<?php echo $userLoggedIn ?>"><?php echo $user['first_name']; ?></a>
         <a href="index.php"><i class="fa fa-home"></i></a>
         <a href="#"><i class="fa fa-envelope"></i></a>
         <a href="#"><i class="fa fa-users"></i></a>
