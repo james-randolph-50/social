c9de6377d4bc24d3b5923f0c3658b1a9c1c0af29
James Randolph <james.randolph2016@gmail.com>
2020-04-06 14:37:06 -0400

    htaccess

---
+++ profile.php
@@ -0,0 +1,13 @@
+<?php
+include("includes/header.php");
+?>
+
+
+<div class="main_column column">
+    This is a profile page
+</div>
+
+
+</div> <!-- closes wrapper from header.php -->
+</body>
+</html>
